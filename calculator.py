a19ef9338d6c1278ab8ef7ac0742d6bd90da4b5d
alishf <ali88206497@gmail.com>
2026-07-24 20:47:40 +0330

    Add calculator.py and test_calculator.py

---
+++ calculator.py
@@ -0,0 +1,21 @@
+class Calculator:
+
+    @staticmethod
+    def addition(a, b):
+        return a + b
+
+     
+    @staticmethod
+    def subtraction(a, b):
+        return a - b
+
+    @staticmethod
+    def multiplication(a, b):
+        return a * b
+
+    @staticmethod
+    def division(a, b):
+        if b == 0:
+            raise ValueError("B can't be zero!!!!")
+        return a / b
+    
---
+++ test_calculator.py
@@ -0,0 +1,32 @@
+import unittest
+from calculator import Calculator
+
+
+class TestCalculator(unittest.TestCase):
+
+    def setUp(self) -> None:
+        """ This method instantiate objects from Calculator class """
+        Calculator() 
+        
+
+
+    def test_addition(self) -> None:
+        # Mahyar
+        pass
+
+    def test_subtraction(self) -> None:
+        # Roozbeh
+        pass
+
+    def test_multiplcation(self) -> None:
+        # Ali
+        pass
+
+    def division(self) -> None:
+        # Arash
+        pass
+
+    def division_by_zero(self) -> None:
+        # Khodam
+        pass
+        
